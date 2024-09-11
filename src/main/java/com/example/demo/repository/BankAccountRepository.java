4658e8fa342d911e8c5089b5d4b553982d57a68f
BabsWade <wadebabacar813@gmail.com>
2024-09-11 10:41:45 +0000

    mise à jour des noms des class

--- src/main/java/com/example/demo/repository/BankAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.BankAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
-}
--- src/main/java/com/example/demo/service/InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-public class InsufficientFundsException extends RuntimeException {
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
